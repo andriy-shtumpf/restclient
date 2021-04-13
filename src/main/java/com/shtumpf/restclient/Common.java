f17590d2fd8f87e8c70dd6243ac77bc6ec5d4bfc
andriyscode <shtumpf@gmail.com>
2021-04-13 13:29:25 -0400

    Initial commit - complete REST Client API

---
+++ src/main/java/com/shtumpf/restclient/Common.java
@@ -0,0 +1,35 @@
+package com.shtumpf.restclient;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.client.RestTemplate;
+
+import com.shtumpf.restclient.models.data.Page;
+
+@ControllerAdvice
+public class Common {
+	
+	@Autowired			
+	private RestTemplate rest;
+	
+	@ModelAttribute
+	public void sharedData(Model model) {
+		
+		ResponseEntity<List<Page>> response = rest.exchange("http://localhost:8080/pages/all", 
+															HttpMethod.GET,	
+															null, 
+															new ParameterizedTypeReference<List<Page>>() {});
+		
+		List<Page> pages = response.getBody();
+		
+		model.addAttribute("cpages", pages);
+	}
+
+}
---
+++ src/main/java/com/shtumpf/restclient/models/PageRepository.java
@@ -0,0 +1,17 @@
+package com.shtumpf.restclient.models;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.shtumpf.restclient.models.data.Page;
+
+public interface PageRepository extends JpaRepository<Page, Integer> {
+	
+	Page findBySlug(String slug);
+	
+	Page findBySlugAndIdNot(String slug, int id);
+	
+	List<Page> findAllByOrderBySortingAsc();
+
+}
